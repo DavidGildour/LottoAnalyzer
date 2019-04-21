4b93479f21d7476d108583d52aed3a430ac4f28d
DavidGildour <davidgildour@mail.com>
2019-04-21 16:17:17 +0200

    changed routes and request method

--- analyzer/lotto.py
+++ analyzer/lotto.py
@@ -1,22 +1,43 @@
-import functools
+# import functools
 
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
-from random import randint
+# from random import randint
 
 from analyzer.db import get_db
 
 bp = Blueprint('lotto', __name__, url_prefix='/lotto')
 
 
-@bp.route('/', methods=("GET",))
+@bp.route('/')
 def index():
     db = get_db().cursor()
 
-    rand = randint(1, 6241)
+    db.execute("SELECT * FROM lotto ORDER BY id DESC LIMIT(1)")
+    latest = db.fetchone()["date"]
 
-    db.execute("SELECT lotto, plus FROM lotto WHERE id=?;", (rand,))
-    latest = db.fetchone()
+    return render_template('lotto/search_results.html', latest=latest)
 
-    return f"Random draw results: {tuple(latest)}"
+
+@bp.route('/date')
+def date():
+    db = get_db().cursor()
+
+    day = request.args.get('day') or '%'
+    month = request.args.get('month') or '%'
+    year = request.args.get('year') or '%'
+
+    searched_date = ".".join([day, month, year])
+
+    db.execute("SELECT id, date, l1||l2||l3||l4||l5||l6 AS lotto, "
+               "p1||p2||p3||p4||p5||p6 AS plus FROM lotto WHERE date LIKE ?", (searched_date,))
+    results = db.fetchall()
+
+    return render_template('lotto/search_results.html', results=results or [])
+
+
+@bp.route('/numbers')
+def numbers():
+
+    return render_template('lotto/search_results.html', results=[])
