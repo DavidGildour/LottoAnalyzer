e86094e667f13a0a25c9247909b509355e53ea88
DavidGildour <davidgildour@mail.com>
2019-04-23 10:38:01 +0200

    small cosmetic change

--- analyzer/db_manager.py
+++ analyzer/db_manager.py
@@ -14,7 +14,7 @@ def connection_handler(func):
 def get_records_by_numbers(cur, numbers):
     param_dict = {str(k): v for k, v in enumerate(numbers)}
     sql = ("select id, date, concat_ws(' ', l1, l2, l3, l4, l5, l6) as lotto, " 
-           "concat_ws(' ', p1, p2, p3, p4, p5, p6) as plus, 'klasyczne' as typ from lotto where ")
+           "concat_ws(' ', p1, p2, p3, p4, p5, p6) as plus, 'lotto' as typ from lotto where ")
     sql += " and ".join([f"%({i})s in (l1,l2,l3,l4,l5,l6)" for i, _ in enumerate(numbers)])
     sql += (" union select id, date, concat_ws(' ', l1, l2, l3, l4, l5, l6) as lotto, " 
             "concat_ws(' ', p1, p2, p3, p4, p5, p6) as plus, 'plus' as typ from lotto where")
